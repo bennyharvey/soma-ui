10da82ad678546d38110f0f6e365cb419265db2d
John Doe <johndoe@example.com>
2020-12-18 07:22:46 +0300

    working on events

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -21,6 +21,11 @@ import InputBase from '@material-ui/core/InputBase';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import SaveIcon from '@material-ui/icons/Save';
+import PlayArrow from '@material-ui/icons/PlayArrow';
+import StopIcon from '@material-ui/icons/Stop';
 
 // const Events = () => {
 //     const classes = layout.makeCommonClasses()
@@ -86,7 +91,8 @@ const useStyles = makeStyles((theme) => ({
       marginRight: theme.spacing(1),
       width: 200,
     },
-  }));
+}));
+
 
 const API = (props) => {
     const [token, setToken] = useState('')
@@ -96,6 +102,10 @@ const API = (props) => {
     const [pageCount, setPageCount] = useState(1)
     const [page, setPage] = useState(1)
 
+    const classes = layout.makeCommonClasses()
+
+    const [photos, setPhotos] = useState([])
+
     // let apiUrl = API_URL
     
 
@@ -103,6 +113,104 @@ const API = (props) => {
         'login': 'admin', 
         'password': 'Cuzee2motof6aiJe'
     }
+
+        
+    // Getting person photos
+    // =================================
+    const retrievePersonPhotos = () => {
+        fetch(config.LOGIN_URL, {
+            method: 'POST',
+            body: JSON.stringify(config.credentials),
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            mode: 'cors',
+            cache: 'default',
+        })
+        .then(res => res.json())
+        .then(
+            (result) => {
+                console.log('photo fetch')
+                console.log(result)
+                let queryToken = '&token=' + result.token
+                // let offset = page  == undefined ? '&limit=10&offset=0' : '&limit=10&offset=' + page * 10
+                let offset = '&limit=10&offset=0'
+
+                fetch(config.PERSONS_URL + '?order_by=time&order_direction=desc' + offset + queryToken, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                })
+                .then(res => res.json())
+                .then(
+                    (personsResponce) => {
+                        console.log(personsResponce)
+                        // setIsLoaded(true)
+                        let itemBuffer = []
+                        let promises = []
+                        personsResponce.forEach(person => (
+                            promises.push(fetchPersonPhotos(person, queryToken).then((result) => {
+                                // console.log(result)
+                                if (result[0] === undefined) {
+                                    return {
+                                        id: '',
+                                        photo_id: '',
+                                    }
+                                }
+                                else {
+                                    itemBuffer[person.id] = result[0].photo_id
+                                    return {
+                                        id: result[0].id ?? '',
+                                        photo_id: result[0].photo_id ?? '',
+                                    }
+                                }
+
+                            }))
+                        ))
+                        Promise.all(promises).then((photos) => {
+                            console.log(itemBuffer)
+                            setPhotos(itemBuffer)
+                        })
+                        // setItems(personsResponce)
+                        // setPageCount(10)
+                    },
+                    (error) => {
+                        console.log(error)
+                        // setIsLoaded(true)
+                        // setError(error)
+                    }
+                )
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+                console.log(error)
+            }
+        )
+
+    }
+    const fetchPersonPhotos = async (person, queryToken) => {
+        let url = config.PERSONS_URL + '/' + person.id + '/faces?' + queryToken
+        let requestOptions = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }
+        const response = await fetch(url, requestOptions);
+        const jsonData = await response.json();
+        // setPersonPhotos(jsonData)
+        // console.log(jsonData)
+        return jsonData
+    }
+
+ 
+
+    
 
     const retrieveItems = () => {
         fetch(config.LOGIN_URL, {
@@ -123,11 +231,9 @@ const API = (props) => {
                 let queryToken = '&token=' + result.token
                 
                 // let offset = page  == undefined ? '&limit=10&offset=0' : '&limit=10&offset=' + page * 10
-                let offset = '&limit=10&offset=0'
-                fetchEvents(offset, queryToken)  
-                setInterval(function(){ 
-                    fetchEvents(offset, queryToken)  
-                }, 500);
+                // let offset = '&limit=10&offset=0'
+                fetchEvents(queryToken)  
+               
                 setIsLoaded(true)
                 setPageCount(10)
 
@@ -151,16 +257,18 @@ const API = (props) => {
             },
             (error) => {
                 console.log(error)
-                setIsLoaded(true)
-                setError(error)
             }
         )
 
     }
 
+    
+
     // const [events, setEvents] = useState([])
 
-    const fetchEvents = async (offset, queryToken) => {
+    const fetchEvents = async (queryToken) => {
+        let offset = page  == undefined ? '&limit=10&offset=0' : '&limit=10&offset=' + page * 10
+        // let offset = '&limit=10&offset=0'
 
         // let page = 1
         // let offset = page  == undefined ? '&limit=10&offset=0' : '&limit=10&offset=' + page * 10
@@ -187,6 +295,8 @@ const API = (props) => {
 
     useEffect(() => {
         setPage(props.page)
+        retrievePersonPhotos()
+
     }, [])
 
     // setTimeout(() => {
@@ -196,8 +306,6 @@ const API = (props) => {
 
     useEffect(retrieveItems, [page])
     
-    const classes = layout.makeCommonClasses()
-    const dynamicPaper = clsx(classes.paper, classes.blurredBackground, classes.animatedBox, classes.zoomIn, classes.paperHover)
 
     const [age, setAge] = React.useState('');
     const handleChange = (event) => {
@@ -241,6 +349,11 @@ const API = (props) => {
         },
       }))(InputBase);
 
+    const [playButtonIcon, setPlayButtonIcon] = React.useState(<PlayArrow />);
+    const [playButtonText, setPlayButtonText] = React.useState('Старт');
+    const [playButtonColor, setPlayButtonColor] = React.useState('primary');
+    const [intervalID, setIntervalID] = React.useState(0);
+
     if (error) {
       return <div>Error: {error.message}</div>
     } else if (!isLoaded) {
@@ -249,7 +362,7 @@ const API = (props) => {
         return (
         <div>
             <Grid container spacing={3}>
-                <Grid item xs={6}>
+                <Grid item xs={4}>
                     <Route>
                         {({ location }) => {
                             const query = new URLSearchParams(location.search)
@@ -275,6 +388,35 @@ const API = (props) => {
                             )
                         }}
                     </Route>
+                    
+                </Grid>
+                <Grid item xs={2}>
+                    <Button
+                        variant="contained"
+                        color={playButtonColor}
+                        size="large"
+                        onClick={() => { 
+                            if (playButtonText == 'Стоп') {
+                                setPlayButtonIcon(<PlayArrow />)
+                                setPlayButtonColor('primary')
+                                setPlayButtonText('Старт')
+                                clearInterval(intervalID)
+                                
+                            } else if (playButtonText == 'Старт') {
+                                setPlayButtonIcon(<StopIcon />)
+                                setPlayButtonColor('default')
+                                setPlayButtonText('Стоп')
+                                let id = setInterval(function() {
+                                    fetchEvents('&token=' + token)  
+                                }, 500);
+                                setIntervalID(id)
+                            }
+                        }}
+                        // className='soma-person-data-confirm_button'
+                        startIcon={playButtonIcon}
+                    >
+                        {playButtonText}
+                    </Button>
                 </Grid>
                 <Grid item xs={2}>
                     <FormControl className={classes.margin}>
@@ -304,15 +446,7 @@ const API = (props) => {
            
             <br />
             {items.map(item => (
-                <div key={item.id}>
-                    <Grid container spacing={3}>
-                        <Grid item xs={12}>
-                            <Paper elevation={5} className={dynamicPaper}>
-                                <PersonBlock data={item} imageSrc={item.avatar} token={token}/>
-                            </Paper>
-                        </Grid>
-                    </Grid>
-                </div>
+                <PersonBlock key={item.id} data={item} imageSrc={item.avatar} token={token}/>
             ))}
         </div>
         )
@@ -322,15 +456,44 @@ const API = (props) => {
 
 
 const PersonBlock = (props) => {
+    const classes = layout.makeCommonClasses()
+    const dynamicPaper = clsx(classes.paper, classes.blurredBackground, classes.animatedBox, classes.zoomIn, classes.paperHover)
 
-
-    // console.log(props)
-    return (
-        <div className='soma-person-block' key={props.id}>
-            <PersonImage src={props.imageSrc} photoId={props.data.data.photo_id} token={props.token}/>
-            <PersonData data={props.data} />
-        </div>
-    )
+    if (props.data.type == 'face_recognize') {
+        // console.log(props)
+        return (
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Paper elevation={5} className={dynamicPaper}>
+                        <div className='soma-person-block' key={props.id}>
+                            <PersonImage src={props.imageSrc} photoId={props.data.data.photo_id} token={props.token} />
+                            <PersonData data={props.data} />
+                        </div>
+                    </Paper>
+                </Grid>
+            </Grid>
+        )
+    }
+    else if (props.data.type == 'person_recognize') {
+        // console.log(props)
+        return (
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Paper elevation={5} className={dynamicPaper}>
+                        <div className='soma-person-block' key={props.id}>
+                            <PersonImage src={props.imageSrc} photoId={props.data.data.photo_id} token={props.token} />
+                            {/* <RecognizedPersonImage src={props.imageSrc} photoId={props.data.data.photo_id} token={props.token} /> */}
+                            <PersonData data={props.data} />
+                        </div>
+                    </Paper>
+                </Grid>
+            </Grid>
+        )
+    }
+    else {
+        return null
+    }
+   
 }
 
 const PersonImage = (props) => {
@@ -342,12 +505,65 @@ const PersonImage = (props) => {
 }
 
 const PersonData = (props) => {
+    let t1 = (props.data.time).split('T')
+    let t2 = t1[1].split('.')
+    let date = t2[0]
+    let time = t1[0]
+
+    let detectConfidence = (props.data.data.detect_confidence).toFixed(2) * 100
+    if (detectConfidence > 100) detectConfidence = detectConfidence - (detectConfidence - 100) //lol
+
+
+    if (props.data.type == 'face_recognize') {
+        return (
+            <div className='soma-person-data'> 
+                <div className='soma-person-data-time'> {date}  {time} </div>
+                <div className='soma-person-data-text'> Нет совпадений </div>
+                <div className='soma-person-data-dc'>Уверенность распознавания: {detectConfidence}% </div>
+                <div className='soma-person-data-id'> #{props.data.id} </div>
+            </div>
+        )
+    } else if (props.data.type == 'person_recognize') {
+        let descriptorsDistance = 100 - (props.data.data.descriptors_distance).toFixed(2) * 100
+        if (descriptorsDistance > 100) descriptorsDistance = descriptorsDistance - (descriptorsDistance - 100)
+        let name = props.data.data.person_name
+        let dossier = props.data.data.person_position
+
+        return (
+            <div className='soma-person-data'> 
+            <div className='soma-person-data-time'> {date}  {time} </div>
+            <div className='soma-person-data-text'> {name}</div>
+            <div className='soma-person-data-text'> Тип досье: {dossier} </div>
+            <div className='soma-person-data-dc'>Уверенность распознавания: {detectConfidence}% </div>
+            <div className='soma-person-data-dc'>Точность совпадения: {descriptorsDistance}% </div>
+            <div className='soma-person-data-dc'>Статус: <span className='soma-person-data-not_confirmed_text'> не подтверждено </span> </div>
+            <Button
+                variant="contained"
+                color="primary"
+                size="large"
+                className='soma-person-data-confirm_button'
+                startIcon={<SaveIcon />}
+            >
+                Подтвердить
+            </Button>
+            <div className='soma-person-data-id'> #{props.data.id} </div>
+        </div>
+        )
+    }
+   
+}
+
+const RecognizedPersonImage = (props) => {
+    let photoID = props.photos[props.id]
+    console.log(props);
+    console.log(photoID);
     return (
-        <div className='soma-person-data'> 
-            {props.data.id} {props.data.time} {props.data.type} {props.data.data.detect_confidence}
+        <div className='soma-person-image'> 
+            <img src={config.PHOTOS_URL + '/' + photoID + '?token=' +props.token} />
         </div>
     )
 }
+
 function DateAndTimePickers(props) {
     const classes = useStyles();
   
@@ -365,7 +581,7 @@ function DateAndTimePickers(props) {
         />
       </form>
     );
-  }
+}
 
 export default Events
 
